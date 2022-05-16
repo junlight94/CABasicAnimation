86fdc18db5725a30e7d4c491e835b5eedbfaa36c
junyoung <junlight94@gmail.com>
2022-05-16 15:14:06 +0900

    end

--- CABasicAnimation/ViewController.swift
+++ CABasicAnimation/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var actionView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let rotation: CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.x")
+        // pi = 180도
+        rotation.toValue = Double.pi * 2
+        // 1바퀴 도는데 걸리는 시간
+        rotation.duration = 2
+        // 반복 횟수 = infinity(무한대)
+        rotation.repeatCount = Float.infinity
+        actionView.layer.add(rotation, forKey: "rotationAnimation")
     }
 
 
